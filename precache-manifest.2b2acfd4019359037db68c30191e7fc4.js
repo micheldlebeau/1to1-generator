6d7bc2c7f96c2cd37552761fb37203f196e3dbc4
Michel Lebeau <michel@ecs-digital.co.uk>
2020-02-21 10:39:09 +0000

    Updates

---
+++ precache-manifest.2b2acfd4019359037db68c30191e7fc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc72c6c622221d40b66fed88041d2ec5",
+    "url": "/1to1-generator/index.html"
+  },
+  {
+    "revision": "da51a97c6e6ee647de3d",
+    "url": "/1to1-generator/static/css/main.592e1ef6.chunk.css"
+  },
+  {
+    "revision": "9f149f299f4a02b61336",
+    "url": "/1to1-generator/static/js/2.912d013d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/1to1-generator/static/js/2.912d013d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da51a97c6e6ee647de3d",
+    "url": "/1to1-generator/static/js/main.4df519b2.chunk.js"
+  },
+  {
+    "revision": "e00618bbe63e07a5c685",
+    "url": "/1to1-generator/static/js/runtime-main.5eca4612.js"
+  }
+]);
